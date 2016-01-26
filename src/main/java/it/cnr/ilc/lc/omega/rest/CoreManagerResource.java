d5db39886cd79915e102e60f5230b97c31e64200
angelo <angelo@dylan2>
2016-01-26 22:00:26 +0100

    Testing in tomcat failed

--- src/main/java/it/cnr/ilc/lc/omega/rest/CoreManagerResource.java
+++ src/main/java/it/cnr/ilc/lc/omega/rest/CoreManagerResource.java
@@ -6,17 +6,16 @@
 package it.cnr.ilc.lc.omega.rest;
 
 import it.cnr.ilc.lc.omega.core.OmegaCore;
+import it.cnr.ilc.lc.omega.core.ResourceManager;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import sirius.kernel.di.std.Part;
 
 /**
  * REST Web Service
@@ -29,7 +28,7 @@ public class CoreManagerResource {
 
     @Context
     private UriInfo context;
-
+  
     /**
      * Creates a new instance of CoreManagerResource
      */
@@ -40,24 +39,32 @@ public class CoreManagerResource {
      * Retrieves representation of an instance of
      * it.cnr.ilc.lc.omega.rest.CoreManagerResource
      *
+     * @param action
      * @return an instance of java.lang.String
      */
     @Path("/{action}")
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String getHtml(
-            @DefaultValue("start") @QueryParam("action") String action
+            @PathParam("action") String action
     ) {
+        System.out.println("parameter in CoreManager " + action);
         String ret = "Microkernel  " + action + " ok";
         //TODO return proper representation object
         if ("start".equals(action)) {
+            System.out.println("starting methods");
             //start MicroKernel
             OmegaCore.start();
         } else {
+            System.out.println("parameter in CoreManager " + action);
             OmegaCore.stop();
             //stop MicroKernel
         }
+        System.out.println("returning getHtml " + ret);
+        
         return ret;
     }
 
 }
+
+// webresources/coremanager/stop
